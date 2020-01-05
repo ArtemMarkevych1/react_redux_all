fc201db2bdf1d1269a05055c237987052bce937d
ArtemMarkevych1 <aw.markevich@gmail.com>
2020-01-05 18:47:52 +0200

    first commit

---
+++ build/precache-manifest.d4fe7ee8d87fc64523df4e8d56930d9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6da9f4fcacf8d13e9fa198c2dde502",
+    "url": "/artemmarkevych1/react_redux_all/index.html"
+  },
+  {
+    "revision": "2ec4d89f6825dc92fe1b",
+    "url": "/artemmarkevych1/react_redux_all/static/css/main.e6cce7a3.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/artemmarkevych1/react_redux_all/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "2ec4d89f6825dc92fe1b",
+    "url": "/artemmarkevych1/react_redux_all/static/js/main.9cd5a4fc.chunk.js"
+  },
+  {
+    "revision": "46d86563967c9b9d99de",
+    "url": "/artemmarkevych1/react_redux_all/static/js/runtime~main.3af295c5.js"
+  }
+]);
